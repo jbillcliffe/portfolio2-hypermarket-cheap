f04d78d4613af4179c0620cc4cf261b79c5e11c7
jbillcliffe <>
2023-09-04 14:24:46 +0000

    (feat)green/red/strike fonts in item

--- assets/js/script.js
+++ assets/js/script.js
@@ -227,34 +227,64 @@ function emptyBasket(requestType = "new") {
 function changeAisle(aisleId, callback) {
     document.getElementById("toolbar-loading").style.display = "flex";
     document.getElementById("loading-overlay").style.display = "block";
+
     //Ensure the shop is emptied each time an aisle button is clicked
     let shopAisle = document.getElementById("the-items");
-
     shopAisle.innerHTML = "";
 
     //Iterate through the stock options and add when its the right aisle option
     for (let stockItem of shopStock) {
 
         if (stockItem.aisle === aisleId) {
+
             //Creating the elements for each aisle item
             const aisleItem = document.createElement("DIV");
             const itemDisplay = document.createElement("DIV");
+            const priceDisplay = document.createElement("DIV");
             const itemImage = document.createElement("IMG");
             const basketAdd = document.createElement("BUTTON");
             const itemPTag = document.createElement("P");
             const stockPTag = document.createElement("P");
+            const pricePTag = document.createElement("P");
+            const innerStockSpan = document.createElement("SPAN");
+            const innerPriceSpan = document.createElement("SPAN");
+            const priceAmountSpan = document.createElement("SPAN");
+            const specialPTag = document.createElement("P");
 
-            //variable text nodes for stock and item text
+            /*
+            Variable text nodes for stock, item, price and 
+            (if necessary) special text
+            */
+            let stockStart = document.createTextNode("Stock :");
             let stockText;
+            let priceStartText = document.createTextNode("Price :");
+            let priceAmountText;
+            let specialText;
             let itemText = document.createTextNode(stockItem.name);
+            let innerPriceText;
 
-            //class names for the main div, text div and button for css styling
+            itemPTag.appendChild(itemText);
+            /*
+            Define class names for the divs that are all present within a
+            singular item div in the shop 
+            */
             aisleItem.className = "aisle-item";
             itemImage.className = "aisle-item-img";
             itemDisplay.className = "aisle-item-p-div";
-            basketAdd.className = "aisle-item-button";
+            priceDisplay.className = "aisle-item-p-div price-div";
             itemPTag.className = "aisle-item-text";
+            stockPTag.className = "aisle-item-text";
+            pricePTag.className = "aisle-item-text";
+            innerPriceSpan.className = "aisle-item-text strikethrough-text";
+            priceAmountSpan.className = "aisle-item-text";
+            specialPTag.className = "aisle-item-text positive-text";
+            basketAdd.className = "aisle-item-button";
 
+            /*
+            ----- IMAGE -----
+            Search for the image in the directory, if found put it in the box,
+            if not, display the no-image 
+            */
             checkIfImageExists("assets/images/items/" + stockItem.imageUrl, (exists) => {
                 if (exists) {
                     itemImage.src = "assets/images/items/" + stockItem.imageUrl;
@@ -263,34 +293,96 @@ function changeAisle(aisleId, callback) {
                 }
             });
 
-            //add to basket button element properties
+            /* ----- STOCK -----
+            Setting an id for the stock text area, then setting its text dependant
+            on whether the stock is 0, or more. If it is 0, the element is given
+            two classes to apply. One, the same as the usual text, the second to
+            override the colour of white and to make it red.
+            */
+            stockPTag.id = "stock_" + stockItem.id;
+
+            if (stockItem.quantity > 0) {
+                stockText = document.createTextNode(stockItem.quantity);
+                innerStockSpan.className = "aisle-item-text";
+            } else {
+                stockText = document.createTextNode("Out Of Stock");
+                innerStockSpan.className = "aisle-item-text negative-text";
+            }
+            innerStockSpan.appendChild(stockText);
+            stockPTag.appendChild(stockStart);
+            stockPTag.appendChild(innerStockSpan);
+            /* ----- PRICING -----
+            { id, name, price, quantity, aisle, special, imageUrl };
+            the "special" of each object in the shopStock array defines its
+            special offer. 
+            If it is 1, then it implies that :
+            Price = price * 1 - Therefore no special offer
+            Price = price * 0.9 - Remove 10% off original price.
+            If the price has a special, it needs to display the RRP, special
+            price and special offer name. Including css formatting for the offer.
+            Also ids for the elements are created here.
+            */
+
+            pricePTag.appendChild(priceStartText);
+
+            if (stockItem.special == 1) {
+
+                priceAmountText = document.createTextNode("£" + stockItem.price);
+                priceAmountSpan.appendChild(priceAmountText);
+                pricePTag.appendChild(priceAmountSpan);
+
+                specialText = document.createTextNode(" ");
+
+            } else {
+                let calculatedAmount = (stockItem.price * stockItem.special).toFixed(2);
+                priceAmountText = document.createTextNode("£" + calculatedAmount.toString());
+                priceAmountSpan.appendChild(priceAmountText);
+                pricePTag.appendChild(priceAmountSpan);
+
+
+                innerPriceText = document.createTextNode("£" + stockItem.price);
+                innerPriceSpan.appendChild(innerPriceText);
+                pricePTag.appendChild(innerPriceSpan);
+                /*
+                Get the name from the specials.json that has been pulled into
+                an array previously
+                */
+                for (let eachSpecial of specialOffers) {
+                    if (eachSpecial.factor === stockItem.special) {
+                        specialText = document.createTextNode(eachSpecial.name);
+                        specialPTag.appendChild(specialText);
+                    } else {
+                        continue;
+                    }
+                }
+
+            }
+
+
+
+
+            /* ----- ADD TO BASKET BUTTON -----
+            Define the ID, put a + inside the button and assign the onClick to
+            each button. Each button is then given the id of the item to send to
+            the basket when it is clicked.
+            */
             basketAdd.id = "basket-add_" + stockItem.id;
             basketAdd.onclick = function () { addToBasket(stockItem.id); };
             basketAdd.innerHTML = "+";
 
-            //stock text property of id, to allow manipulation when stock changes
-            stockPTag.id = "stock_" + stockItem.id;
-
-            //Check if there is stock available at the start, if there is none,
-            //display out of stock text
-            if (stockItem.quantity > 0) {
-                stockText = document.createTextNode("Stock : " + stockItem.quantity);
-                stockPTag.className = "aisle-item-text";
-            } else {
-                stockText = document.createTextNode("Out Of Stock. Come back another day");
-                stockPTag.className = "aisle-item-text no-stock";
-            }
             /*
-                Appending to the document. Text nodes to paragraphs, paragraphs
-                to text div, the text div to the aisle div and then the button to 
-                the aisle div
+            Appending to the document. Text nodes to paragraphs, paragraphs
+            to text div, the text div to the aisle div and then the button to 
+            the aisle div
             */
-            itemPTag.appendChild(itemText);
-            stockPTag.appendChild(stockText);
             itemDisplay.appendChild(itemPTag);
             itemDisplay.appendChild(stockPTag);
+            priceDisplay.appendChild(pricePTag);
+            priceDisplay.appendChild(specialPTag);
+
             aisleItem.appendChild(itemImage);
             aisleItem.appendChild(itemDisplay);
+            aisleItem.appendChild(priceDisplay);
             aisleItem.appendChild(basketAdd);
 
             //add the item to the shop
@@ -304,6 +396,7 @@ function changeAisle(aisleId, callback) {
 
     callback();
 }
+
 /**
  * @param {number} itemId - id of the item passed to the function from shopStock
  * 
